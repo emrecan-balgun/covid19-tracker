46f5113a10464862e0e6975524aefc2f5b49ff19
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-10 02:26:10 +0300

    refactor countries and add nanoid

--- src/Container/Countries.js
+++ src/Container/Countries.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { nanoid } from 'nanoid'
 
 function Countries() {
+    const [countries, setCountries] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios('https://restcountries.com/v3.1/all')
+        .then(response => setCountries(response.data))
+        .catch(e => console.log(e))
+        .finally(() => setIsLoading(false))
+    }, []);
+
     return (
         <div>
-            <select name="cars" id="cars">
-                <option value="" disabled selected hidden>Global</option>
+            <select defaultValue={'default'}>
                 {
-                    // https://restcountries.com
+                    isLoading ? <option value="default">Loading..</option> : <option value="default" disabled  hidden>Global</option>
+                }
+                {
+                    countries.map(country => <option key={nanoid()} value={country.name.common}>{country.name.common}</option>)
                 }
             </select>
         </div>
