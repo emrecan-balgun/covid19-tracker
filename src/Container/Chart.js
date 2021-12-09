49c75c8da6dbdf3c9683160288fb7821965cd7e2
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-09 23:25:46 +0300

    add container

---
+++ src/Container/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chart() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chart
---
+++ src/Container/Information.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Information() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Information
