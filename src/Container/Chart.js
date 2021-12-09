844f5c1513fea8c16e87b444700c22cc7aa0974c
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-10 01:54:33 +0300

    add chartjs and create chart.js, chart.scss

--- src/Container/Chart.js
+++ src/Container/Chart.js
@@ -1,11 +1,79 @@
 import React from 'react'
+import { Bar
+} from 'react-chartjs-2'
 
-function Chart() {
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  } from 'chart.js';
+  
+  Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  );
+
+
+function Charts() {
+    const state = {
+        labels: ["Confirmed", "Recovered", "Deaths"],
+        datasets:
+            [{
+                label: "Infected",
+                backgroundColor: ["red", "green","blue"],
+                borderColor: "rgba(0,0,0,0)",
+                borderWidth: "1",
+                data: ["8986377", "0", "78602"]
+            }]
+    }
     return (
         <div className="chart">
-            
+            <h1>Chart</h1>
+            <Bar data={state}/>
         </div>
     )
 }
 
-export default Chart
+export default Charts
--- src/Container/Information.js
+++ src/Container/Information.js
@@ -10,15 +10,13 @@ function Information() {
         <div className="information">
             <h2 className="information__title">Global Data</h2>
             <span className="information__time">Last update: 10.12.2021 00:15</span>
-            {/* <select name="cars" id="cars">
+            <select name="cars" id="cars">
                 <option value="" disabled selected hidden>Global</option>
-                <option value="turkey">Turkey</option>
-                <option value="usa">USA</option>
-                <option value="germany">Germany</option>
-                <option value="france">France</option>
-            </select> */}
+                {
+                    // https://restcountries.com
+                }
+            </select>
             <Box />
-
         </div>
         <Chart />
         </>
