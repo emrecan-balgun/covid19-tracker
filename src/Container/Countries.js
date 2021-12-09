b6f7cf771e4e0aa18d15eb3caed95c69ba05dc09
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-10 02:07:44 +0300

    add axios and create countries.js, countries.scss

---
+++ src/Container/Countries.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Countries() {
+    return (
+        <div>
+            <select name="cars" id="cars">
+                <option value="" disabled selected hidden>Global</option>
+                {
+                    // https://restcountries.com
+                }
+            </select>
+        </div>
+    )
+}
+
+export default Countries
--- src/Container/Information.js
+++ src/Container/Information.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Box from './Box'
 import Table from './Table'
 import Header from './Header'
+import Countries from './Countries'
 
 function Information() {
     return (
@@ -10,12 +11,7 @@ function Information() {
         <div className="information">
             <h2 className="information__title">Global Data</h2>
             <span className="information__time">Last update: 10.12.2021 00:15</span>
-            <select name="cars" id="cars">
-                <option value="" disabled selected hidden>Global</option>
-                {
-                    // https://restcountries.com
-                }
-            </select>
+            <Countries />
             <Box />
         </div>
         <Table />
