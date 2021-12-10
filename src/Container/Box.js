6fb9cad20e3982d1cf8faedac40b1e84a748ffd2
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-11 01:56:50 +0300

    refactor box.js and add number format

--- src/Container/Box.js
+++ src/Container/Box.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { globalConfirmed, globalRecovered, globalDeaths } from '../redux/covidSlice';
 
 function Box() {
+    const confirmed = useSelector(globalConfirmed);
+    const recovered = useSelector(globalRecovered);
+    const deaths = useSelector(globalDeaths);
+
+    let numberFormat = new Intl.NumberFormat('en-us');
+
     return (
         <div className="boxes">
             <div className="boxes__confirmed">
-                <span className="boxes__confirmed__value">268,393,653</span>
+                <span className="boxes__confirmed__value">{numberFormat.format(confirmed)}</span>
                 <span className="boxes__confirmed__name">confirmed</span>
             </div>
 
             <div className="boxes__recovered">
-                <span className="boxes__recovered__value">0</span>
+                <span className="boxes__recovered__value">{numberFormat.format(recovered)}</span>
                 <span className="boxes__recovered__name">recovered</span>
             </div>
 
             <div className="boxes__deaths">
-                <span className="boxes__deaths__value">5,285,485</span>
+                <span className="boxes__deaths__value">{numberFormat.format(deaths)}</span>
                 <span className="boxes__deaths__name">deaths</span>
             </div>
             
