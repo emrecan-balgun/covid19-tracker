0cba8f4be963bd670f451a3365fb97197ae51d89
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-14 13:26:55 +0300

    refactor darkMode.js

--- src/Container/DarkMode.js
+++ src/Container/DarkMode.js
@@ -1,22 +1,21 @@
-import { useState } from 'react'
 import moon from '../Assets/moon.png';
 import sun from '../Assets/sun.png';
+import { useSelector, useDispatch } from 'react-redux';
+import { darkMode, changeDarkMode } from '../redux/covidSlice';
 
 function DarkMode() {
-    const [state, setstate] = useState(false)
+    const mode = useSelector(darkMode);
+    const dispatch = useDispatch();
 
     return (
-        <>
-        
-        <button className="darkMode" onClick={() => setstate(!state)}>
+        <button className="darkMode" onClick={() => dispatch(changeDarkMode(!mode))}>
             {
-                state === true 
+                mode === true 
                 ? <img className="darkMode__image" src={moon} alt="moon" /> 
                 : <img className="darkMode__image" src={sun} alt="sun" />
             }
             
         </button>
-        </>
     )
 }
 
